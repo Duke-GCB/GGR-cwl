e3ca3b8dd4728e99598bd67136ff6375ac59f621
Dan Leehr <dan.leehr@duke.edu>
2015-09-16 10:38:54 -0400

    Revert "Change path to use STAR 2.4.1a"
    This reverts commit 9bc0e7bd53608fd2a1427c9eb0638e74e4231a04.

--- ggr-rna-seq.sh
+++ ggr-rna-seq.sh
@@ -6,5 +6,5 @@
 #SBATCH --mail-type=FAIL
 
 source ~/env-cwl/bin/activate
-export PATH="/data/reddylab/software/FastQC:/home/dcl9/bin:/usr/local/bin:/data/reddylab/software/STAR-STAR_2.4.1a/bin/Linux_x86_64_static:$PATH"
+export PATH="/data/reddylab/software/FastQC:/home/dcl9/bin:/usr/local/bin:/data/reddylab/software/STAR_2.4.2a/STAR-STAR_2.4.2a/bin/Linux_x86_64_static:$PATH"
 srun cwltool --debug --preserve-environment PATH --outdir ~/ggr-cwl-data --no-container ~/ggr-cwl/ggr-rna-seq.cwl ~/ggr-cwl/rna-seq-hardac.json
